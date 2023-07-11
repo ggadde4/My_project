9d8182e90d79757050c28f73fb74c6b9ed71a5fe
giridhargadde <giridhargadde@macbook-pro.lan>
2023-07-11 17:07:20 -0400

    added maven project.

---
+++ src/test/java/com/assignment/my_assignment/AppTest.java
@@ -0,0 +1,38 @@
+package com.assignment.my_assignment;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+    @Test
+    public void shouldAnswerWithTrue01()
+    {
+        assertFalse( false );
+    }
+    @Test
+    public void shouldAnswerWithTrue02()
+    {
+        assertEquals( true,true );
+    }
+    @Test
+    public void shouldAnswerWithTrue03()
+    {
+        assertNotEquals( true,false );
+    }
+}
